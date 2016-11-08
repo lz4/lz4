0f73ed5726d1d9fac33f00a4440b2e3953fb5bd6
Przemyslaw Skibinski <inikep@gmail.com>
2016-11-08 09:19:09 +0100

    new Travis tests

--- programs/lz4cli.c
+++ programs/lz4cli.c
@@ -59,20 +59,17 @@
 #include "lz4.h"      /* LZ4_VERSION_STRING */
 
 
-/****************************
+/*-************************************
 *  OS-specific Includes
-*****************************/
-#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(_WIN32)
-#  include <io.h>       /* _isatty */
-#  if defined(__DJGPP__)
-#    include <unistd.h>
-#    define _isatty isatty
-#    define _fileno fileno
-#  endif
-#  define IS_CONSOLE(stdStream) _isatty(_fileno(stdStream))
-#else
+**************************************/
+#if defined(_POSIX_SOURCE) || defined(_POSIX_C_SOURCE) || defined(_XOPEN_SOURCE) || (defined(__APPLE__) && defined(__MACH__)) || defined(__DJGPP__)  /* https://sourceforge.net/p/predef/wiki/OperatingSystems/ */
 #  include <unistd.h>   /* isatty */
 #  define IS_CONSOLE(stdStream) isatty(fileno(stdStream))
+#elif defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__)
+#  include <io.h>       /* _isatty */
+#  define IS_CONSOLE(stdStream) _isatty(_fileno(stdStream))
+#else
+#  define IS_CONSOLE(stdStream) 0
 #endif
 
 
