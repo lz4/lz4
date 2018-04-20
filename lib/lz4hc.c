ca833f928f64d0b88fbfd2a8a8bc11b8333e487d
W. Felix Handte <w@felixhandte.com>
2018-04-20 14:16:27 -0400

    Also Reset the Chain Table

--- lib/lz4hc.c
+++ lib/lz4hc.c
@@ -98,7 +98,7 @@ static void LZ4HC_init (LZ4HC_CCtx_internal* hc4, const BYTE* start)
     uptrval startingOffset = hc4->end - hc4->base;
     DEBUGLOG(4, "LZ4HC_init(%p, %p)", hc4, start);
     if (startingOffset > 1 GB || startingOffset > (uptrval)start) {
-        MEM_INIT((void*)hc4->hashTable, 0, sizeof(hc4->hashTable));
+        LZ4HC_clearTables(hc4);
         startingOffset = 0;
     }
     startingOffset += MAX_DISTANCE;
