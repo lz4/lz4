49073aca8606131bb73ca1471219d63b27d2602f
Lzu Tao <taolzu@gmail.com>
2018-12-02 20:39:47 +0700

    clang: Fix -Wcomma

--- tests/frametest.c
+++ tests/frametest.c
@@ -160,7 +160,7 @@ static unsigned FUZ_highbit(U32 v32)
 {
     unsigned nbBits = 0;
     if (v32==0) return 0;
-    while (v32) v32 >>= 1, nbBits ++;
+    while (v32) {v32 >>= 1; nbBits ++;}
     return nbBits;
 }
 
