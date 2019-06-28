e2a33f12e1198d3f5de374519d7a034a8736f6c8
Max Dymond <cmeister2@gmail.com>
2019-06-29 00:23:06 +0100

    More markups for style changes

--- ossfuzz/compress_fuzzer.c
+++ ossfuzz/compress_fuzzer.c
@@ -10,17 +10,16 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   size_t const compressed_dest_size = LZ4_compressBound(size);
   char *const dest_buffer = (char *)malloc(compressed_dest_size);
 
-  if (dest_buffer != NULL)
-  {
-    // Allocation succeeded, try compressing the incoming data.
-    int result = LZ4_compress_default((const char*)data,
-                                      dest_buffer,
-                                      size,
-                                      compressed_dest_size);
-    CHECK(result != 0);
+  CHECK(dest_buffer != NULL);
 
-    free(dest_buffer);
-  }
+  // Allocation succeeded, try compressing the incoming data.
+  int result = LZ4_compress_default((const char*)data,
+                                    dest_buffer,
+                                    size,
+                                    compressed_dest_size);
+  CHECK(result != 0);
+
+  free(dest_buffer);
 
   return 0;
 }
--- ossfuzz/decompress_fuzzer.c
+++ ossfuzz/decompress_fuzzer.c
@@ -7,22 +7,22 @@
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
+  // TODO: Size input buffer pseudo-randomly based on seed extracted from input
   size_t const buffer_size = 10 * 1024 * 1024;
   char *const dest_buffer = (char *)malloc(buffer_size);
 
-  if (dest_buffer != NULL)
-  {
-    // Allocation succeeded, try decompressing the incoming data.
-    int result = LZ4_decompress_safe((const char*)data,
-                                     dest_buffer,
-                                     size,
-                                     buffer_size);
+  CHECK(dest_buffer != NULL);
 
-    // Ignore the result of decompression.
-    (void)result;
+  // Allocation succeeded, try decompressing the incoming data.
+  int result = LZ4_decompress_safe((const char*)data,
+                                   dest_buffer,
+                                   size,
+                                   buffer_size);
 
-    free(dest_buffer);
-  }
+  // Ignore the result of decompression.
+  (void)result;
+
+  free(dest_buffer);
 
   return 0;
 }
