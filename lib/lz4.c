8c8f0eb4baf1bb65411534a2c009746340b0f1f3
Bimba Shrestha <bimbashrestha@fb.com>
2020-02-06 14:43:58 -0800

    Turning on fast dec for visual

--- lib/lz4.c
+++ lib/lz4.c
@@ -374,7 +374,7 @@ static const int      dec64table[8] = {0, 0, 0, -1, -4,  1, 2, 3};
 
 
 #ifndef LZ4_FAST_DEC_LOOP
-#  if defined __i386__ || defined _M_IX86 || defined __x86_64__ || defined _M_X64
+#  if defined _MSC_VER || defined __i386__ || defined _M_IX86 || defined __x86_64__ || defined _M_X64
 #    define LZ4_FAST_DEC_LOOP 1
 #  elif defined(__aarch64__) && !defined(__clang__)
      /* On aarch64, we disable this optimization for clang because on certain
