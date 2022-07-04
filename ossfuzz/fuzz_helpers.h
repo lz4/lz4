f745a01cfd6363093de97e6969661cbe99e2393d
Yann Collet <cyan@fb.com>
2022-07-04 16:36:19 -0700

    clarify yet another time what dual-license means

--- ossfuzz/fuzz_helpers.h
+++ ossfuzz/fuzz_helpers.h
@@ -4,7 +4,8 @@
  *
  * This source code is licensed under both the BSD-style license (found in the
  * LICENSE file in the root directory of this source tree) and the GPLv2 (found
- * in the COPYING file in the root directory of this source tree).
+ * in the COPYING file in the root directory of this source tree),
+ * meaning you may select, at your option, one of the above-listed licenses.
  */
 
 /**
