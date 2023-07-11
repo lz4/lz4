ccef95b8216243d77789c749e34b9aeff22c8cfa
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2023-07-11 18:17:11 +0900

    fix: issue #1248
    Fix issue #1248, reported by @BojackMa.

--- examples/fileCompress.c
+++ examples/fileCompress.c
@@ -41,6 +41,7 @@ static int compress_file(FILE* f_in, FILE* f_out)
     void* const buf = malloc(CHUNK_SIZE);
     if (!buf) {
         printf("error: memory allocation failed \n");
+        return 1;
     }
 
     /* Of course, you can also use prefsPtr to
