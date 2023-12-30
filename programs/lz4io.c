bde2e95db7a22d50e9b7d968d0322dffe6f8dada
Yann Collet <cyan@fb.com>
2023-12-29 18:57:24 -0800

    fixed initialization issue

--- programs/lz4io.c
+++ programs/lz4io.c
@@ -523,7 +523,7 @@ static void WR_destroy(WriteRegister* wr)
 static WriteRegister WR_init(size_t blockSize)
 {
     WriteRegister wr = { 0, NULL, WR_INITIAL_BUFFER_POOL_SIZE, 0, 0 };
-    wr.buffers = (BufferDesc*)malloc(WR_INITIAL_BUFFER_POOL_SIZE * sizeof(BufferDesc));
+    wr.buffers = (BufferDesc*)calloc(1, WR_INITIAL_BUFFER_POOL_SIZE * sizeof(BufferDesc));
     wr.blockSize = blockSize;
     return wr;
 }
