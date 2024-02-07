80026581de24b86b3fb3fec100ddee07faa79ad8
Yann Collet <cyan@fb.com>
2024-02-06 22:03:56 -0800

    long matches look for 7 bytes
    instead of 8,
    which is _generally_ favorable.

--- lib/lz4hc.c
+++ lib/lz4hc.c
@@ -110,7 +110,10 @@ static U64 LZ4_read64(const void* memPtr)
 #define LZ4MID_HASHSIZE 8
 #define LZ4MID_HASHLOG (LZ4HC_HASH_LOG-1)
 #define LZ4MID_HASHTABLESIZE (1 << LZ4MID_HASHLOG)
-static U32 LZ4MID_hash8(U64 v) { return (U32)((v * 0xCF1BBCDCB7A56463ULL) >> (64-LZ4MID_HASHLOG)); }
+
+//static U32 LZ4MID_hash8(U64 v) { return (U32)((v * 0xCF1BBCDCB7A56463ULL) >> (64-LZ4MID_HASHLOG)); }
+static U32 LZ4MID_hash8(U64 v) { return (U32)(((v  << (64-56)) * 58295818150454627ULL) >> (64-LZ4MID_HASHLOG)) ; }
+
 static U32 LZ4MID_hash8Ptr(const void* ptr) { return LZ4MID_hash8(LZ4_read64(ptr)); }
 static U32 LZ4MID_hash4(U32 v) { return (v * 2654435761U) >> (32-LZ4MID_HASHLOG); }
 static U32 LZ4MID_hash4Ptr(const void* ptr) { return LZ4MID_hash4(LZ4_read32(ptr)); }
