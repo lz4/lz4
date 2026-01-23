e48d59f1848dd5ca0ba9d2f991a6be3dee4d1705
Jenn <jenlee68@meta.com>
2026-01-23 14:57:01 -0800

    Add updatE

--- lib/lz4.c
+++ lib/lz4.c
@@ -2202,7 +2202,7 @@ LZ4_decompress_generic(
             cpy = op + length;
 
             assert((op <= oend) && (oend-op >= 32));
-            if (unlikely(offset<16)) {
+            if (unlikely(offset<16) && length > offset) {
                 LZ4_memcpy_using_offset(op, match, cpy, offset);
             } else {
                 LZ4_wildCopy32(op, match, cpy);
