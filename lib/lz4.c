e2184d880ebd0bc81ae6cd87ffbf101a8a3e4e67
Jenn <jenlee68@meta.com>
2026-01-26 11:11:13 -0800

    Add fuzzing fix

--- lib/lz4.c
+++ lib/lz4.c
@@ -2202,8 +2202,13 @@ LZ4_decompress_generic(
             cpy = op + length;
 
             assert((op <= oend) && (oend-op >= 32));
-            if (unlikely(offset<16) && length > offset) {
-                LZ4_memcpy_using_offset(op, match, cpy, offset);
+            if (unlikely(offset<16)) {
+                if (length > offset){
+                    LZ4_memcpy_using_offset(op, match, cpy, offset);
+                }else{
+                    /* no overlap possible use simple memcpy*/
+                    LZ4_memcpy(op, match, length);
+                }
             } else {
                 LZ4_wildCopy32(op, match, cpy);
             }
