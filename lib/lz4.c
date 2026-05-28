1eef5480afdc2f3c4add896d03e56bb60c9aca95
Nicolas De Carli <nikodecarli@gmail.com>
2026-05-28 11:17:54 -0400

    Unroll literal length decoding logic
    Peel the first literal load, potentially allowing skipping overflow checks

--- lib/lz4.c
+++ lib/lz4.c
@@ -2102,19 +2102,21 @@ LZ4_decompress_generic(
 
             /* decode literal length */
             if (length == RUN_MASK) {
-                size_t s;
-                do {
-                    s = *ip++;
-                    if (unlikely(ip >= iend)) { goto _output_error; } /* overflow detection */
-                    length += s;
-                    if ((sizeof(length)<8) && unlikely(length > ((Rvl_t)(-1)/2)) ) {
-                        goto _output_error;
-                    }
-                } while (s==255);
+                size_t s = *ip++;
+                length += s;
+                if (s == 255) {
+                    do {
+                        s = *ip++;
+                        if (unlikely(ip >= iend)) { goto _output_error; } /* overflow detection */
+                        length += s;
+                        if ((sizeof(length)<8) && unlikely(length > ((Rvl_t)(-1)/2)) ) {
+                            goto _output_error;
+                        }
+                    } while (s == 255);
+                    if (unlikely((uptrval)(op)+length<(uptrval)(op))) { goto _output_error; } /* overflow detection */
+                    if (unlikely((uptrval)(ip)+length<(uptrval)(ip))) { goto _output_error; } /* overflow detection */
+                }
                 cpy = op+length;
-                if (unlikely((uptrval)(cpy)<(uptrval)(op))) { goto _output_error; } /* overflow detection */
-                if (unlikely((uptrval)(ip)+length<(uptrval)(ip))) { goto _output_error; } /* overflow detection */
-
                 /* copy literals */
                 LZ4_STATIC_ASSERT(MFLIMIT >= WILDCOPYLENGTH);
               #ifdef __aarch64__
